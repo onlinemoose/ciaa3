2a4ebcb095a1d46add55c080bf2537acae26ee5f
Markus Smet <markussmet@hotmail.com>
2023-04-11 18:34:03 +0100

    Site added to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   // your configuration options here...
   // https://docs.astro.build/en/reference/configuration-reference/
+  site: 'https://taichiforeverybody.com/'
 })
