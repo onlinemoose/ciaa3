2a33bbe2361bdf497f0679f256a551c4b7b6e402
Markus Smet <markussmet@hotmail.com>
2023-04-11 18:03:28 +0100

    Remaining Internal Arts content added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
-
 export default defineConfig(
     /** @type {import('astro').AstroUserConfig} */
     {
